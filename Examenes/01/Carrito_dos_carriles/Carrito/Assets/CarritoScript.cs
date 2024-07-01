74cc4bb4d0aa56a04ea890a243b0492e7d1caf58
JACKSON ALEXANDER RAMON RAMON <jackson.ramon@epn.edu.ec>
2024-07-01 06:05:16 -0500

    Se agrega el contenido del examen del primer bimestre

---
+++ Examenes/01/Carrito_dos_carriles/Carrito/Assets/CarritoScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarritoScript : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 200f;
+    [SerializeField] float moveSpeed = 15f;
+
+    [SerializeField] float velocidadRapido = 20f;
+    [SerializeField] float velocidadLento = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Rapido")
+        {
+            Debug.Log("Ir Rapido");
+            moveSpeed = velocidadRapido;
+        }
+        if (other.tag == "Lento")
+        {
+            Debug.Log("Ir Lento");
+            moveSpeed = velocidadLento;
+        }
+    }
+}
